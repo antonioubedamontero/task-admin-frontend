93d9e9cee14ab2120451db8b193cfe662d1579c8
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-06-28 08:39:15 +0200

    extract language to service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
-import { AVAILABLE_LANGS, DEFAULT_LANG } from './available-langs';
+import { LanguageService } from './auth/services/language.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {
   protected title = 'Task Admin';
 
-  translate = inject(TranslateService);
+  language = inject(LanguageService);
 
   constructor() {
     this.configureLangs();
   }
 
   private configureLangs(): void {
-    // Add available langs and configure languages
-    this.translate.addLangs([...AVAILABLE_LANGS]);
-    this.translate.setDefaultLang(DEFAULT_LANG);
-    this.translate.use(DEFAULT_LANG);
-
-    // Update language translations with browser lang if available
-    const browserLang = this.translate.getBrowserLang();
-    if (browserLang && AVAILABLE_LANGS.includes(browserLang)) {
-      this.translate.use(browserLang);
-    }
+    this.language.initializeLanguages();
+    const browserLang = this.language.browserLang;
+    this.language.changeLanguage(browserLang);
   }
 }
