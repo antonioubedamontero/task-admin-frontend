6ced2a26993530cd0afa470f06bb1a4fd799f8dc
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-08-01 15:52:03 +0200

    add message service test

--- src/app/shared/services/message.service.spec.ts
+++ src/app/shared/services/message.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MessageService } from './message.service';
+import { ModelUserFeedbackType } from '../interfaces';
 
 describe('MessageService', () => {
   let service: MessageService;
@@ -12,5 +13,31 @@ describe('MessageService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should open a modal message with a title and description', () => {
+    service.showModal(
+      'Modal Title',
+      'Operation go well',
+      ModelUserFeedbackType.success
+    );
+    expect(service.isModalShown()).toBeTruthy();
+    expect(service.modalTitle()).toBe('Modal Title');
+    expect(service.modalType()).toBe(ModelUserFeedbackType.success);
+    expect(service.modalText()).toBe('Operation go well');
+  });
+
+  it('should closeModal', () => {
+    service.showModal(
+      'Modal Title',
+      'Operation go well',
+      ModelUserFeedbackType.WARNING
+    );
+
+    service.closeModal();
+    expect(service.isModalShown()).toBeFalsy();
+    expect(service.modalType()).toBe(ModelUserFeedbackType.success);
+    expect(service.modalTitle()).toBe('');
+    expect(service.modalText()).toBe('');
   });
 });
