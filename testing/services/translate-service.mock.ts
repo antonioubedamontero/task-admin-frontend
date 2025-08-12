c8de1c5c05954b5c5c870bb4e826731b962201b2
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-08-12 21:56:15 +0200

    add more tests

--- testing/services/translate-service.mock.ts
+++ testing/services/translate-service.mock.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AVAILABLE_LANGS, DEFAULT_LANG } from '../../src/app/available-langs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +8,37 @@ import { Injectable } from '@angular/core';
 export class TranslateServiceMock {
   instant(key: string): string {
     return `mock translation ${key}`;
+  }
+
+  get langs(): string[] {
+    return [...AVAILABLE_LANGS];
+  }
+
+  addLangs(langs: string[]): void {
+    return;
+  }
+
+  setDefaultLang(lang: string) {
+    return;
+  }
+
+  getDefaultLang(): string {
+    return DEFAULT_LANG;
+  }
+
+  getBrowserLang(): string | undefined {
+    return DEFAULT_LANG;
+  }
+
+  get currentLang(): string {
+    return DEFAULT_LANG;
+  }
+
+  set currentLang(currentLang: string) {
+    return;
+  }
+
+  use(lang: string): Observable<string> {
+    return of(lang);
   }
 }
