4944b4f08615d7c8156f9be603d6baa921bf1db5
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-08-14 10:58:44 +0200

    add some more tests

--- testing/components/index.ts
+++ testing/components/index.ts
@@ -2,3 +2,4 @@ export * from './auth-layout.component.mock';
 export * from './task-layout.component.mock';
 export * from './modal-user-feedback.component.mock';
 export * from './mini-task-buttons.components.mock';
+export * from './mini-task.component.mock';
--- testing/data/index.ts
+++ testing/data/index.ts
@@ -3,3 +3,4 @@ export * from './register-response-data';
 export * from './task-response-data';
 export * from './tasks-respponse-data';
 export * from './delete-response.data';
+export * from './mini-task-item.data';
